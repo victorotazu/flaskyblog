c0250e5d81b0ff5dfc3386671c0c595914104841
Victor <votazu.estrada@gmail.com>
2019-02-04 12:56:34 -0500

    Add user registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,3 +27,16 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, 
+                    username=form.username.data, 
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
