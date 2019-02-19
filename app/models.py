bcc53620724e10d70b509571d7169dd48e956fbe
Victor <votazu.estrada@gmail.com>
2019-02-19 11:38:40 -0500

    Add user profiles and editor

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from . import db, login_manager
+from datetime import datetime
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -65,6 +66,11 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
     
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -122,6 +128,11 @@ class User(UserMixin, db.Model):
         return self.role is not None and self.role.has_permission(perm)
     def is_administrator(self):
         return self.can(Permission.ADMIN)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
+        db.session.commit()
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, perm):
